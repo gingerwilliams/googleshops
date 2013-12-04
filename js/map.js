d554a486913c3f92a380115bd8186104d74296e4
GingerWilliams <geedimensions@gmail.com>
2013-12-04 12:27:52 -0500

    Adding a weather layer and corresponding library

--- js/map.js
+++ js/map.js
@@ -1,48 +1,30 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see a blank space instead of the map, this
-// is probably because you have denied permission for location sharing.
-
-var map;
-
 function initialize() {
-  //ADDING THE MAP
   var mapOptions = {
-    center: new google.maps.LatLng(37.7831,-122.4039),
-    zoom: 12,
+    zoom: 6,
+    center: new google.maps.LatLng(49.265984,-123.127491),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  var map = new google.maps.Map(document.getElementById('map'),
+      mapOptions);
 
   //ADDING LOCATION TYPE + AUTOCOMPLETE TEXTFIELD FEATURE
   var acOptions = {
-  	types: ['establishment']
+    types: ['establishment']
   }
 
   var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),acOptions);
   autocomplete.bindTo('bounds',map);
 
   //ADDING A MARKER
-  // var markersOptions = {
-  // 	position: new google.maps.LatLng(37.7831,-122.4039)
-  // };
-
   var marker = new google.maps.Marker({
     map: map
   });
-  // marker.setMap(map);
 
   //ADDING INFO WINDOW
-  // var infoWindowOptions = {
-  // 	content: 'Barbershop Info Window'
-  // }
-
   var infoWindow = new google.maps.InfoWindow();
 
-  // google.maps.event.addListener(marker,'click', function(e){
-  // 	infoWindow.open(map, marker);
-  // });
-
+  //ADDING AN EVENT LISTENER FOR THE CLICK EVENT
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
   infoWindow.close();
   var place = autocomplete.getPlace();
@@ -62,13 +44,14 @@ function initialize() {
     });
   });
 
+  //ADDING WEATHER LAYER
+  var weatherLayer = new google.maps.weather.WeatherLayer({
+    temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
+  });
+  weatherLayer.setMap(map);
+
+  var cloudLayer = new google.maps.weather.CloudLayer();
+  cloudLayer.setMap(map);
 }
 
-
-
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-
-
